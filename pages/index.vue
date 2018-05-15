af8aa60736099ea0f2b13b9396c37dcb8f1734d1
hyunkyuchoi <chlgusrb6@gmail.com>
2018-05-15 10:47:16 +0900

    [2018-05-15] DogLove #1

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+ <section class="hero is-medium is-primary is-bold">
+   <div class = "hero">
+   <div class = "hero-body">
+   <div class = "container">
+   <h1 class = "title is-1">개사랑</h1>
+   <h2 class = "subtitile">개사랑 사이트는 개를 사랑하는 사람들을 위한 상세한 정보를 제공합니다.</h2>
+   </div>
+   </div>
+   </div>
+   <article class ="message">
+   <div class ="message-header">개란 무엇인가?</div>
+   <div class ="message-body"></div>
+  </section>
+</template>
+
+<script>
+
+</script>
+
+
